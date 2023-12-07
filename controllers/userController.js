bfd9efc2a8c3b14b5df85bc1b7f2420558fcadd3
DESKTOP-JFCQK15\User <s108213039@mail1.ncnu.edu.tw>
2023-12-07 23:51:35 +0800

    update - signup API

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,23 +7,49 @@ router.use(bodyParser.json());
 
 class UserController{
   signup = async (req, res) => {
-    const { userId, username, gender, email, phone, password } = req.body;        // 取得用戶輸入資料
-    const duplicateUser = await userModel.readUserId(userId); // 帳號重複檢查
+    const { userId, username, gender, email, phone, password, scanRes} = req.body;  // 取得用戶輸入資料
     try {
+      const duplicateUser = await userModel.readUserId(userId); // 帳號重複檢查
       if (duplicateUser) {
-        res.json({ "result": "Duplicate User" });
-      } else {
-        const result = await userModel.createUser(userId, username, gender, email, phone, password);
-        if (result) {
-          res.json(result);
-        } else {
-          res.status(405).json({ "result": "signup fail" });
-        }
+        res.json({
+          success: false,
+          error: {
+            message: "Signup fail: duplicate user",
+          }
+        });
+      } else if (userId === scanRes){ // 檢查學生證掃描結果
+          const result = await userModel.createUser(userId, username, gender, email, phone, password);  // 創建使用者
+          if (result) {
+            res.json({
+              success: true,
+              // data: result
+            });
+          } else {
+            res.json({
+              success: false,
+              error: {
+                message: "Signup fail : DB error",
+              }
+            });
+          }  
+      }
+      else {
+        res.json({
+          success: false,
+          error: {
+            message: "Signup fail: ID mismatch",
+          }
+        });
       }
     } catch (error) {
-      res.status(405).json({ "result": "signup fail" });
+      res.status(405).json({
+        success: false,
+        error: {
+          message: "Signup fail",
+        }
+      });
       console.log(error);
-    }      
+    }
   }
 
   login = async (req, res) => {
--- models/userModel.js
+++ models/userModel.js
@@ -2,24 +2,6 @@ const sql = require('../mysql.js');
 
 // 定義使用者模型
 class UserModel {
-  async createUser(userId, username, gender, email, phone, password){ // 註冊
-    
-    return new Promise((resolve, reject) => {
-      const query = 'INSERT INTO `user` (`user_id`, `username`, `gender`, `email`, `phone`, `password`) VALUES (?,?,?,?,?,?)';
-      const params = [userId, username, gender, email, phone, password];
-    
-      sql.pool.query(query, params, (error, result) => {
-        if (error) {  // 如果發生錯誤，將錯誤信息傳遞給回呼函數
-          console.log('createUser query 失敗');
-          reject(error);
-        } else {  // 成功時將查詢結果傳遞給回呼函數
-          console.log('createUser query 成功', result);
-          resolve(result);
-        }
-      });
-    })
-  }
-
   async readUserId(userId){ // 檢查重複使用者
     return new Promise((resolve, reject) => {
       const query = 'SELECT * FROM user WHERE user_id = ?';
@@ -27,14 +9,31 @@ class UserModel {
 
       sql.pool.query(query, params, (error, result) => {
         if (error) {  // 如果發生錯誤，將錯誤信息傳遞給回呼函數
-          console.log('readUser query 失敗');
           reject(error);
-        } else {  // 成功時將查詢結果傳遞給回呼函數
-          console.log('readUser query 成功');  
-          if(result.length != 0){
+        } else { 
+          if(result.length != 0){ // 帳號已存在
             resolve(true);
-          } else { resolve(false);}
+          } else { resolve(false);} // 帳號不存在
         }
+      });
+    })
+  }
+
+  async createUser(userId, username, gender, email, phone, password){ // 註冊
+    return new Promise((resolve, reject) => {
+      bcrypt.hash(password, 10).then(function (hash) { // 加密密碼
+        const query = 'INSERT INTO `user` (`user_id`, `username`, `gender`, `email`, `phone`, `password`) VALUES (?,?,?,?,?,?)';
+        const params = [userId, username, gender, email, phone, hash];
+      
+        sql.pool.query(query, params, (error, result) => {
+          if (error) {
+            console.log('createUser query 失敗');
+            reject(error);
+          } else {
+            console.log('createUser query 成功', result);
+            resolve(result);
+          }
+        });
       });
     })
   }
