f4ef230af62011da2cbc1eb2041ac5b3e37ee4ac
DESKTOP-JFCQK15\User <s108213039@mail1.ncnu.edu.tw>
2023-12-16 03:20:56 +0800

    update - signup API. applyForDriver API

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,7 @@ router.use(bodyParser.json());
 
 class UserController{
   signup = async (req, res) => {
-    const { userId, username, gender, email, phone, password, scanRes} = req.body;  // 取得用戶輸入資料
+    const { userId, username, gender, email, phone, password} = req.body;  // 取得用戶輸入資料
     try {
       const duplicateUser = await userModel.readUserId(userId); // 帳號重複檢查
       if (duplicateUser) {
@@ -21,7 +21,7 @@ class UserController{
             message: "Signup fail: duplicate user",
           }
         });
-      } else if (userId === scanRes){ // 檢查學生證掃描結果
+      } else {
           const result = await userModel.createUser(userId, username, gender, email, phone, password);  // 創建使用者
           if (result) {
             res.json({
@@ -36,14 +36,6 @@ class UserController{
               }
             });
           }  
-      }
-      else {
-        res.json({
-          success: false,
-          error: {
-            message: "Signup fail: ID mismatch",
-          }
-        });
       }
     } catch (error) {
       res.status(405).json({
@@ -169,7 +161,7 @@ class UserController{
   }
 
   applyForDriver = async (req, res) => {
-    const { userId, carId, seat, charge } = req.body;        // 取得用戶輸入資料
+    const { userId, carId, seat, charge, category } = req.body;        // 取得用戶輸入資料
     try {
       const driverExist = await activityModel.readDriverId(userId); // 檢查司機是否存在
       if (driverExist) {
@@ -180,7 +172,7 @@ class UserController{
           }
         });
       } else {
-        const result = await userModel.createDriver(userId, carId, seat, charge); // 創建司機身分
+        const result = await userModel.createDriver(userId, carId, seat, charge, category); // 創建司機身分
         if (result){
           res.json({
             success: true,
@@ -195,7 +187,7 @@ class UserController{
         }
       }  
     } catch (error) {
-      res.status(405).json({ "result": "login fail" });
+      res.status(405).json({ "result": "Apply For Driver fail" });
     }      
   }
 
